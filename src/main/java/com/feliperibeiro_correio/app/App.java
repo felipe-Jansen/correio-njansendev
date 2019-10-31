274be42bbac1ed0229c5c92fba13123d31e512d2
Felipe Jansen <felipe.jansen@otimizeit.com.br>
2019-10-31 14:18:35 -0300

    Alterações

--- src/main/java/com/feliperibeiro_correio/app/App.java
+++ src/main/java/com/feliperibeiro_correio/app/App.java
@@ -1,6 +1,5 @@
 package com.feliperibeiro_correio.app;
 
-import com.feliperibeiro_correio.app.services.CalculaFrete;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -12,18 +11,5 @@ import java.io.IOException;
  */
 public class App {
     public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
-//        System.out.println(CalculaFrete.getCalcPrecoPrazo("","", "04030","56318000","56318000",
-//                "20","20","20","20","20","20","mao","20","aviso").getPrazoEntrega());
-//        System.out.println(CalculaFrete.getCalcPrazoData("04715","56318000","56318000","").getPrazoEntrega());
-//        System.out.println(CalculaFrete.getCalcPrazo("04715","56318000","56318000").getPrazoEntrega());
-//        System.out.println(CalculaFrete.getCalcPrecoData("","", "04030","56318000","56318000",
-//                "20","20","20","20","20","20","mao","20","aviso", "").getValor());
-//        System.out.println(CalculaFrete.getCalcPrecoPrazoData("", "", "04030", "56318000", "56318000",
-//                "20", "20", "20", "20", "20", "20", "mao", "20", "aviso", "").getValor());
-//         System.out.println(CalculaFrete.getPrecoPrazoRestricao("", "", "04030", "56318000", "56318000",
-//                "20", "20", "20", "20", "20", "20", "mao", "20", "aviso", "").getValor());
-        // System.out.println(CalculaFrete.getCalcPrazoRestricao("04715","56318000","56318000","").getPrazoEntrega());
-//        System.out.println(CalculaFrete.getCalcPreco("", "", "04030", "56318000", "56318000",
-//                "20", "20", "20", "20", "20", "20", "mao", "20", "aviso").getValor());
     }
 }
